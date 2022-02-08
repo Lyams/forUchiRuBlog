7d799d95cceb987b929829fdbdc8026c3773b2bd
Ilia Liamshin <lyamsh@yandex.ru>
2022-02-09 00:05:57 +0300

    init: configure rails to use uuid instead

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  # Sort records by date of creation instead of primary key
+  self.implicit_order_column = :created_at
 end
