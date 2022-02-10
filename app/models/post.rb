98766fb6cef243a15495e1f47d0ad83a6198a6db
Ilia Liamshin <lyamsh@yandex.ru>
2022-02-10 08:32:22 +0300

    feat: Added validation to Post Model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Post < ApplicationRecord
-  validates :title, :body, presence: true
+  validates :body, presence: true, length: { minimum: 2 }
+  validates :title, presence: true, length: { minimum: 1, maximum: 25 }
   has_many :comments, dependent: :destroy
 end
