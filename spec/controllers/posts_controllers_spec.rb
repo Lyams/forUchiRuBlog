79d27ce8ef11351ecaef6e227af866dedfaa709e
Ilia Liamshin <lyamsh@yandex.ru>
2022-02-10 09:49:59 +0300

    test&info: added tests; deleted pending tests

--- spec/controllers/posts_controllers_spec.rb
+++ spec/controllers/posts_controllers_spec.rb
@@ -38,20 +38,30 @@ RSpec.describe PostsController, type: :controller do
   end
 
   describe 'GET#edit' do
-    subject { process :edit, method: :get, params: {id: post.id} }
+    subject { process :edit, method: :get, params: { id: post.id } }
     it 'created post' do
       is_expected.to render_template :edit
-      expect{ subject }.to_not change { Post.count }
+      expect { subject }.to_not change { Post.count }
       expect(assigns :post).to eq post
     end
   end
 
   describe '#destroy' do
-    let!(:new_post){create :post}
-    subject { delete :destroy, params: {id: new_post} }
+    let!(:new_post) { create :post }
+    subject { delete :destroy, params: { id: new_post } }
     it 'destroys Post record' do
       expect { subject }.to change { Post.count }.by(-1)
       expect(response).to redirect_to posts_url
+    end
+  end
+
+  describe '#update' do
+    let!(:old_post) { create :post }
+    let(:params) { { id: old_post, post: { id: old_post.id, body: 'New decription! ;)' } } }
+    subject { process :update, method: :patch, params: params }
+    it 'update Post record' do
+      expect { subject }.to_not change { Post.count }
+      expect(old_post.reload.body).to eq('New decription! ;)')
     end
   end
 end
