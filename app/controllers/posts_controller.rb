d63baae81d3fd08da7adb791f0bc71e9d53d54c2
Ilia Liamshin <lyamsh@yandex.ru>
2022-03-11 03:38:17 +0300

    feat: turboframe for New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,6 +30,7 @@ class PostsController < ApplicationController
     respond_to do |format|
       if @post.save
         format.html { redirect_to post_url(@post), notice: 'Post was successfully created.' }
+        format.turbo_stream { flash.now[:success] = 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
         format.html { render :new, status: :unprocessable_entity }
